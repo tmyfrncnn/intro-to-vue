89c9a3e488481f3469b754d9ba6a86e0a2a207e3
Téa May Francinne <tmyfrncnn@gmail.com>
2023-09-06 13:35:38 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Sock is a textile garment worn on the foot for comfort, protection, and style. Sock is made from various materials and come in different lengths and designs, making them essential for everyday wear and fashion.'
+        }
+    }
+})
