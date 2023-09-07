077fbf0b9853c0549ef9ee59b83f2c405f969e91
Téa May Francinne <tmyfrncnn@gmail.com>
2023-09-07 11:56:40 +0800

    created vue app, applied attribute binding and conditional rendering.

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'Sock is a textile garment worn on the foot for comfort, protection, and style. Sock is made from various materials and come in different lengths and designs, making them essential for everyday wear and fashion.'
+            description: 'Sock is a textile garment worn on the foot for comfort, protection, and style. Sock is made from various materials and come in different lengths and designs, making them essential for everyday wear and fashion.',
+
+            image: './assets/images/socks_green.jpg',
+
+            url: 'https://socks.com.au',
+
+            inventory: 14,
+
+            onSale: true,
         }
     }
 })
