97febe1818a6a721a400f8e8adff49062be82830
Téa May Francinne <tmyfrncnn@gmail.com>
2023-09-07 13:30:54 +0800

    added 'event handling'

--- main.js
+++ main.js
@@ -11,6 +11,15 @@ const app = Vue.createApp({
             inventory: 14,
 
             onSale: true,
+
+            details: ['50% cotton', '30% wool', '20% polyester'],
+
+            sizes: ['Small', 'Medium', 'Large', 'Extra Large', 'Extra Extra Large', 'Yeti'],
+
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color:' blue' },
+            ],
         }
     }
 })
