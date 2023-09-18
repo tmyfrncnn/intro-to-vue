be378ee0e019030ba5c9dfcc827dde337325feec
Téa May Francinne <tmyfrncnn@gmail.com>
2023-09-18 21:01:40 +0800

    added 'event handling'

--- main.js
+++ main.js
@@ -1,25 +1,42 @@
 const app = Vue.createApp({
-    data() {
+    data: function() {
         return {
-            product: 'Socks',
-            description: 'Sock is a textile garment worn on the foot for comfort, protection, and style. Sock is made from various materials and come in different lengths and designs, making them essential for everyday wear and fashion.',
-
+            cart:0,
+            product: "Socks",
+            description: "Sock is a textile garment worn on the foot for comfort, protection, and style. Sock is made from various materials and come in different lengths and designs, making them essential for everyday wear and fashion.",
+    
             image: './assets/images/socks_green.jpg',
-
+    
             url: 'https://socks.com.au',
-
+    
             inventory: 14,
-
+    
             onSale: true,
-
+    
             details: ['50% cotton', '30% wool', '20% polyester'],
-
-            sizes: ['Small', 'Medium', 'Large', 'Extra Large', 'Extra Extra Large', 'Yeti'],
-
+    
+            sizes: ['S', 'M', 'L', 'XL',],
+    
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color:' blue' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ],
         }
-    }
+    },
+
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+    
+        removeFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1;
+            }
+        },
+
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+    },
 })
