70929b467c15b31defc04351a231591bae74112f
Téa May Francinne <tmyfrncnn@gmail.com>
2023-09-18 21:03:00 +0800

    added 'event handling'

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 const app = Vue.createApp({
-    data: function() {
+    data() {
         return {
             cart:0,
             product: "Socks",
             description: "Sock is a textile garment worn on the foot for comfort, protection, and style. Sock is made from various materials and come in different lengths and designs, making them essential for everyday wear and fashion.",
-    
+
             image: './assets/images/socks_green.jpg',
-    
+
             url: 'https://socks.com.au',
-    
+
             inventory: 14,
-    
+
             onSale: true,
-    
+
             details: ['50% cotton', '30% wool', '20% polyester'],
-    
+
             sizes: ['S', 'M', 'L', 'XL',],
-    
+
             variants: [
                 { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
                 { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
