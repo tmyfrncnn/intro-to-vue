cb1410823f7ae4c44d1d29de5b0c917085ef860c
Téa May Francinne <tmyfrncnn@gmail.com>
2023-09-20 15:20:57 +0800

    added 'computed properties'

--- main.js
+++ main.js
@@ -3,25 +3,24 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: "Socks",
+            brand: 'Vue Mastery',
             description: "Sock is a textile garment worn on the foot for comfort, protection, and style. Sock is made from various materials and come in different lengths and designs, making them essential for everyday wear and fashion.",
 
-            image: './assets/images/socks_blue.jpg',
-
-            inStock: true,
+            selectedVariant: 0,
 
             url: 'https://socks.com.au',
 
             inventory: 17,
 
-            onSale: false,
+            onSale: true,
 
             details: ['50% cotton', '30% wool', '20% polyester'],
 
             sizes: ['S', 'M', 'L', 'XL',],
 
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 25 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ],
         }
     },
@@ -37,8 +36,26 @@ const app = Vue.createApp({
             }
         },
 
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
         },
     },
+
+    computed: {
+        title () {
+            return this.brand + ' ' + this.product
+        },
+        image () {
+            return this.variants [this.selectedVariant].image
+        },
+        inStock () {
+            return this.variants [this.selectedVariant].quantity
+        },
+        sale () {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + 'is on sale.'
+            }
+            return ''
+        }
+    }
 })
